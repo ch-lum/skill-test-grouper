475dcdd12a4fd79d9c04807eb13174dd4ba76e77
Christopher Lum <chrlum03@gmail.com>
2024-11-27 06:04:33 -0800

    initial commit

---
+++ app/review/[category]/page.tsx
@@ -0,0 +1,3 @@
+export default function CategoryPage() {
+  return <div>Content specific to this category</div>;
+}
---
+++ app/review/layout.tsx
@@ -0,0 +1,20 @@
+type Props = {
+  children: React.ReactNode;
+  params: {
+    category?: string; // Dynamic parameter for the layout
+  };
+};
+
+
+export default function ReviewLayout({ children, params }: Props) {
+  const category = params.category || "Miscellaneous";
+
+  return (
+    <div>
+      <header>
+        <h1>Reviewing Category: {category}</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
