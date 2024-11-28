8429d1ea8b31175bdda605b7d46d6f5e052f2ee9
Christopher Lum <chrlum03@gmail.com>
2024-11-27 17:30:57 -0800

    mix between layout and page

--- app/questions/[question]/review/[category]/page.tsx
+++ app/questions/[question]/review/[category]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useParams, useRouter } from "next/navigation";
 import { useCategories } from "@/context/CategoriesContext";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CodeBlock from "@/app/ui/CodeBlock";
 
 export default function ReviewPage() {
@@ -19,8 +19,6 @@ export default function ReviewPage() {
     return <div>Category not found</div>;
   }
 
-  const categoryTitle = categories.find((cat) => cat.slug === categorySlug)?.title;
-
   // Enforce type safety
   const questionEntries = questionData?.[question] || {};
   if (typeof questionEntries !== 'object') {
@@ -30,7 +28,7 @@ export default function ReviewPage() {
   // Get the code snippets for the emails in emails
   const codeSnippets = emails.map(email => questionEntries[email]).filter(Boolean);
   const [currentSnippetIndex, setCurrentSnippetIndex] = useState<number>(0);
-  
+
   // Adjust points just in case
   const defaultDeduction = categories.find((cat) => cat.slug === categorySlug)?.default_deduction || -1;
   const [pointDeduction, setPointDeduction] = useState<number>(defaultDeduction);
@@ -43,11 +41,9 @@ export default function ReviewPage() {
 
   const handleNext = () => {
     setPointDeduction(defaultDeduction);
-
-    // Navigate to the next code snippet
-    if (currentSnippetIndex < codeSnippets.length - 1) {
-      setCurrentSnippetIndex(currentSnippetIndex + 1);
-    } else {
+    setCurrentSnippetIndex(currentSnippetIndex + 1);
+    
+    if (currentSnippetIndex >= codeSnippets.length - 1) {
       // Handle the case when there are no more snippets
       console.log("No more snippets");
       // Find next category
@@ -55,6 +51,7 @@ export default function ReviewPage() {
       const nextCategorySlug = categories[currentCategoryIndex + 1]?.slug;
       if (nextCategorySlug) {
         router.push(`/questions/${question}/review/${nextCategorySlug}`);
+        setCurrentSnippetIndex(0);
       } else {
         // No more categories
         console.log("No more categories");
@@ -73,40 +70,39 @@ export default function ReviewPage() {
   }
 
   return (
-    <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-semibold mb-6">Reviewing Category: {categoryTitle}</h1>
+    <div>
       <div className="relative mb-8">
-      <div className="absolute top-0 right-0 bg-gray-200 text-gray-800 font-bold py-1 px-3 rounded-bl-lg z-10">
+        <div className="absolute top-0 right-0 bg-gray-200 text-gray-800 font-bold py-1 px-3 rounded-bl-lg z-10">
           {currentSnippetIndex + 1} / {codeSnippets.length}
         </div>
         <div className="code-snippet bg-violet-900 p-4 rounded-lg mb-8 relative z-0">
           <CodeBlock code={codeSnippets[currentSnippetIndex]} language="python" />
         </div>
       </div>
-      <div className="mt-8">
-        <h2 className="text-2xl font-semibold mb-4">Is this classified correctly?</h2>
-        <div className="flex items-center space-x-4">
-          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-            No
-          </button>
-          <button onClick={handleYes} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            Yes
-          </button>
-          <div className="flex items-center space-x-2">
-            <span>Deduction: </span>
-            <input
-              type="number"
-              min={-1}
-              step={0.1}
-              max={0}
-              value={pointDeduction}
-              onChange={handlePointDeductionChange}
-              className="border border-gray-300 rounded py-2 px-4"
-              placeholder="Point Deduction"
-            />
+        <div className="mt-8">
+          <h2 className="text-2xl font-semibold mb-4">Is this classified correctly?</h2>
+          <div className="flex items-center space-x-4">
+            <button onClick={handleNo} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+              No
+            </button>
+            <button onClick={handleYes} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+              Yes
+            </button>
+            <div className="flex items-center space-x-2">
+              <span>Deduction: </span>
+              <input
+                type="number"
+                min={-1}
+                step={0.1}
+                max={0}
+                value={pointDeduction}
+                onChange={handlePointDeductionChange}
+                className="border border-gray-300 rounded py-2 px-4"
+                placeholder="Point Deduction"
+              />
+            </div>
           </div>
         </div>
-      </div>
     </div>
   );
 }
--- app/questions/[question]/review/layout.tsx
+++ app/questions/[question]/review/layout.tsx
@@ -1,23 +1,28 @@
 "use client";
 
+import { useParams } from "next/navigation";
+import { useCategories } from "@/context/CategoriesContext";
+
 type Props = {
   children: React.ReactNode;
-  params: {
-    question: string
-    category: string; // Dynamic parameter for the layout
-  };
 };
 
 
-export default function ReviewLayout({ children, params }: Props) {
-  const category = params.category || "Miscellaneous";
+export default function ReviewLayout({ children, }: Props) {
+  const { categories } = useCategories();
+  const params = useParams();
+
+  // Category data below //
+  const categorySlug = params.category as string
+
+  const categoryTitle = categories.find((cat) => cat.slug === categorySlug)?.title;
+  const categoryDescription = categories.find((cat) => cat.slug === categorySlug)?.description;
 
   return (
-    <div>
-      <header>
-        <h1>Reviewing Category: {category}</h1>
-      </header>
-      <main>{children}</main>
+    <div className="container mx-auto p-8">
+      <h1 className="text-3xl font-semibold mb-6">Reviewing Category: {categoryTitle}</h1>
+      <p className="text-lg mb-6">{categoryDescription}</p>  
+      {children}
     </div>
   );
 }
