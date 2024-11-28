d4494c1723c2c8c8d1ddba2e855c87c90f7f62f6
Christopher Lum <chrlum03@gmail.com>
2024-11-27 19:37:17 -0800

    category numbers

--- app/questions/[question]/review/layout.tsx
+++ app/questions/[question]/review/layout.tsx
@@ -18,9 +18,12 @@ export default function ReviewLayout({ children, }: Props) {
   const categoryTitle = categories.find((cat) => cat.slug === categorySlug)?.title;
   const categoryDescription = categories.find((cat) => cat.slug === categorySlug)?.description;
 
+  const categoryIndex = categories.findIndex((cat) => cat.slug === categorySlug);
+  const categoryTotal = categories.length;
+
   return (
     <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-semibold mb-6">Reviewing Category: {categoryTitle}</h1>
+      <h1 className="text-3xl font-semibold mb-6">Reviewing Category {categoryIndex + 1}/{categoryTotal}: {categoryTitle}</h1>
       <p className="text-lg mb-6">{categoryDescription}</p>  
       {children}
     </div>
